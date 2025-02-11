b9055f0494a6eef179b6279842b1dfe17f79fa36
wangll <1091392350@qq.com>
2025-02-11 10:59:58 +0800

    feat:获取首页

--- src/controllers/aiController.js
+++ src/controllers/aiController.js
@@ -13,7 +13,6 @@ exports.AiDeepSeek = async (ctx) => {
       messages: [{ role: "system", content: `"工作窗口变成“收银台”，湖南一贪官将个人收款码贴到政府办事大厅，开出3000多张假票",找出这段话关键词,并且以数组形式输出,只输出关键词` }],
       model: "deepseek-chat",
     });
-
     const regex = /\[(.*?)\]/;
     const match = completion.choices[0].message.content.match(regex);
 
@@ -34,21 +33,25 @@ exports.AiRyfTool = async (ctx) => {
     const result = await axios.get('https://www.ruanyifeng.com/blog/weekly/');
     const $ = cheerio.load(result.data);
     const ulList = [];
-
     $('h3 + ul.module-list').each(function () {
       // 在当前ul标签内部选择所有的a标签
-      $(this).find('li a').each(function () {
+      $(this).find('li>a').each(function () {
         // 获取并打印a标签的href属性值
+       
         const href = $(this).attr('href');
         ulList.push(href);
       });
     });
-
-
-    ctx.body = ulList;
+    const allPage = []
+    ulList.slice(0,2).forEach( (item) => {
+      allPage.push(axios.get(item)) ;
+    })
+    const allResult = await Promise.all(allPage);
+    console.log(allResult[0].data,'allResult')
+    ctx.body = allResult[0].data;
   } catch (error) {
     ctx.status = 500;
-    ctx.body = 'Internal Server Error';
+    ctx.body = `Internal Server Error: ${error}`;
   }
 
 
